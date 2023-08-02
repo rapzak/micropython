db2ace5b355a8acc16d62eeb3069a017268de6eb
rene <fake@fake.com>
2023-08-02 17:38:10 +0200

    added 8685

---
+++ ports/esp32/boards/ESP8685H2_USB/mpconfigboard.h
@@ -0,0 +1,8 @@
+// This configuration is for a generic ESP8685H2 board with 2MiB flash.
+
+#define MICROPY_HW_BOARD_NAME               "ESP8685H2 Chip"
+#define MICROPY_HW_MCU_NAME                 "ESP8685H2"
+
+#define MICROPY_HW_ENABLE_SDCARD            (0)
+#define MICROPY_PY_MACHINE_DAC              (0)
+#define MICROPY_PY_MACHINE_I2S              (0)
